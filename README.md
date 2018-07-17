3a2e2db9ae0d65436f4f5dbd78fbd39968202129
Nakell Williams <nakellwilliams@FBK-Mac.local>
2018-07-17 09:59:41 -0400

    css

--- README.md
+++ README.md
@@ -1,1 +1,56 @@
 # Psychic-Game
+var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",]
+var winCounter = 0;
+var lossCounter = 0;
+var guessCounter = 10;
+var guessTracker = [];
+var randomLetter;
+
+
+function random(){
+    randomLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
+    console.log(randomLetter);
+    return randomLetter;
+}
+
+random();
+
+
+document.onkeyup = function(event, randomLetter) {
+    var userGuess = event.key;
+    computerGuess = randomLetter;
+    console.log(computerGuess);
+    
+    if (userGuess !== computerGuess) {
+        alert ("Try again");
+        guessCounter --;
+        guessTracker = guessTracker + event.key + ", ";
+        
+    } else if (userGuess === computerGuess) {
+        alert ("Congratulations");
+        winCounter ++;
+        guessCounter = 10;
+        guessTracker = [];
+        computerGuess = random();
+        
+    }
+
+
+    if (guessCounter === 0) {
+        alert ("You lose. Sorry buddy");
+        lossCounter ++;
+        guessCounter = 10;
+        guessTracker = [];
+    }
+  
+    
+    var html =
+          "<h2>Guess what I'm thinking of!</h2>" +
+          "<h2>Winner: " + winCounter + "</h2>" +
+          "<h2>Losses: " + lossCounter + "</h2>" +
+          "<h2>Guesses left: " + guessCounter + "</h2>" +
+          "<h2>Previous Guesses: " + guessTracker + "</h2>" +
+          "<h2>Open the console to see the answer ;)</h2>";
+        // Set the inner HTML contents of the #game div to our html string
+        document.querySelector("#game").innerHTML = html;
+}
---
+++ assets/javascript/game.js
@@ -0,0 +1,68 @@
+
+        var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+        var winCount = 0;
+        var lossCount = 0;
+        var guessCount = 9;
+        var userSelection = [];
+        var randomletter;
+
+        function random() {
+            randomLetter = letters[Math.floor(Math.random() * letters.length)];
+            console.log(randomLetter);
+            return randomLetter;
+
+        }
+        randomLetter = random();
+
+
+        document.onkeyup = function (event) {
+
+            var userGuess = event.key;
+            var compGuess = randomletter;
+            console.log(compGuess);
+
+            var guessOptions = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+
+            
+                if (guessOptions.indexOf(userGuess) > -1){ 
+                        
+                       
+                         if (userGuess === compGuess) {
+                            winCount ++;
+                            guessCount = 9;
+                            userSelection = [];
+                            compGuess = random();
+                        }
+                       if (userGuess !== compGuess){
+                            guessCount --;
+                            userSelection = userSelection + event.key ;
+                        }
+                    
+
+                        if (guessCount === 0) {
+                            guessCount = 9;
+                            lossCount ++;
+                            userSelection = [];
+                        }
+
+                        }
+
+                var html =
+                    "<h1> The Psychic Game </h1>" +
+                    "<p> Guess what letter I'm thinking of </p>" +
+                    "<p> Wins: " + winCount + "</p>" +
+                    "<p> Losses: " + lossCount + "</p>" +
+                    "<p> Guesses Left: " + guessCount + "</p>" +
+                    "<p> Your Guesses so far: " + userSelection + "</p>";
+             // Set the inner HTML contents of the #game div to our html string
+                document.querySelector("#game").innerHTML = html;
+            }
+
+
+
+        ;
+
+
+    
